04590d3c36f565d7bc31a605047594add70cceea
simmitiwari770-beep <simmitiwari770@gmail.com>
2026-03-17 20:33:52 +0530

    Create Ex1.5.py

---
+++ Ex1.5.py
@@ -0,0 +1,11 @@
+#Given an array arr[]. The task is to find the largest element and return it.
+class Solution:
+    def largest(self, arr):
+        max_val=arr[0]
+        
+        for i in range(1,len(arr)):
+            if arr[i]>max_val:
+                max_val=arr[i]
+                
+        return max_val
+        
