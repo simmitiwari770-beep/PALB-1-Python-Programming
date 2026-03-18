8f29f63e1d516b0b29e8e8672dc88e319cd05fdc
simmitiwari770-beep <simmitiwari770@gmail.com>
2026-03-18 20:29:47 +0530

    Create Ex1.6.py

---
+++ Ex1.6.py
@@ -0,0 +1,9 @@
+#Given an array arr, rotate the array by one position in clockwise direction.
+class Solution:
+    def rotate(self, arr):
+        last = arr[-1]          
+        
+        for i in range(len(arr) - 1, 0, -1):
+            arr[i] = arr[i - 1]   
+        
+        arr[0] = last          
