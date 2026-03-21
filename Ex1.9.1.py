0429e383e47bbfc3670afe78ea230494f6095975
simmitiwari770-beep <simmitiwari770@gmail.com>
2026-03-21 19:59:42 +0530

    Create Ex1.9.1.py

---
+++ Ex1.9.1.py
@@ -0,0 +1,31 @@
+#You are given an array arr[] of non-negative numbers. Each number tells you the maximum number of steps you can jump forward from that position.
+class Solution:
+    def minJumps(self, arr):
+        n = len(arr)
+
+        if n <= 1:
+            return 0
+
+        if arr[0] == 0:
+            return -1
+
+        max_reach = arr[0]
+        step = arr[0]
+        jump = 1
+
+        for i in range(1, n):
+            if i == n - 1:
+                return jump
+
+            max_reach = max(max_reach, i + arr[i])
+            step -= 1
+
+            if step == 0:
+                jump += 1
+
+                if i >= max_reach:
+                    return -1
+
+                step = max_reach - i
+
+        return -1
