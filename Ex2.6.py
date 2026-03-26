f13966e1bd62fc0f6814d39da13040f03a34ca2b
Simmi Tiwari <simmitiwari770@gmail.com>
2026-03-26 13:53:33 +0530

    Create Ex2.6.py

---
+++ Ex2.6.py
@@ -0,0 +1,18 @@
+#Given an integer n, find its factorial. Return a list of integers denoting the digits that make up the factorial of n.
+class Solution:
+    def factorial(self, n):
+        result = [1]
+
+        for x in range(2, n + 1):
+            carry = 0
+
+            for i in range(len(result)):
+                product = result[i] * x + carry
+                result[i] = product % 10
+                carry = product // 10
+
+            while carry:
+                result.append(carry % 10)
+                carry //= 10
+
+        return result[::-1]
