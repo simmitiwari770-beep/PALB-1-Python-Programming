19096dd048e9f9a9fc5668e1b65d13d34c6c9885
Simmi Tiwari <simmitiwari770@gmail.com>
2026-03-28 00:48:49 +0530

    Create Ex4.1.py

---
+++ Ex4.1.py
@@ -0,0 +1,17 @@
+#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
