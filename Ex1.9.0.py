85719faafc0103218a6266ff0f67f4233675ea62
Simmi Tiwari <simmitiwari770@gmail.com>
2026-05-14 21:31:35 +0530

    Fix formatting of the twoSum method in Ex1.9.0.py

--- Ex1.9.0.py
+++ Ex1.9.0.py
@@ -1,7 +1,7 @@
 #Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
+     seen = {}
 
         for i in range(len(nums)):
             complement = target - nums[i]
